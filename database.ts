785e1ac1f94f6edd8291e6a201e30d0048234f00
Kameq <90651206+kameqdev@users.noreply.github.com>
2024-10-02 02:06:54 +0200

    try catch enclosure for all db functions

--- database.ts
+++ database.ts
@@ -10,39 +10,56 @@ const api_v4 = await new Client().connect({
 })
 
 
-export async function getPoints(steamID: string): Promise<number> {
-    const { rows } = await api_v4.execute("SELECT points FROM points WHERE steam_id = ?", [steamID])
-
-    return rows?.[0]?.points ?? 0
+export async function getPoints(steamID: string): Promise<number | boolean> {
+    try {
+        const { rows } = await api_v4.execute("SELECT points FROM points WHERE steam_id = ?", [steamID])
+        return rows?.[0]?.points ?? 0
+    } catch (e) {
+        console.log(e)
+        return false
+    }
 }
 
 export async function addPoints(steamID: string, points: number): Promise<boolean> {
-    const res = await api_v4.execute(`
-        INSERT INTO points (steam_id, points)
-        VALUES (?, ?)
-        ON DUPLICATE KEY UPDATE
-        points = points + ?`, 
-        [steamID, points, points])
+    try {
+        const res = await api_v4.execute(`
+            INSERT INTO points (steam_id, points)
+            VALUES (?, ?)
+            ON DUPLICATE KEY UPDATE
+            points = points + ?`, 
+            [steamID, points, points])
 
-    const success = !!res?.affectedRows
+        const success = !!res?.affectedRows
 
-    if (success)
-        api_v4.execute(`
-            INSERT INTO points_logs (steam_id, points_added) VALUES (?, ?)`, [steamID, points])
-    
-    return success
+        if (success)
+            api_v4.execute(`
+                INSERT INTO points_logs (steam_id, points_added) VALUES (?, ?)`, [steamID, points])
+        
+        return success
+    } catch (e) {
+        console.log(e)
+        return false
+    }
 }
 
 export async function bundlePurchase(steamID: string, bundleCode: string, price: number): Promise<boolean> {
-    const res = await api_v4.execute(`INSERT INTO bundles_purchased (steam_id, bundle, price) VALUES (?, ?, ?)`, [steamID, bundleCode, price])
-    
-    return !!res?.affectedRows
+    try {
+        const res = await api_v4.execute(`INSERT INTO bundles_purchased (steam_id, bundle, price) VALUES (?, ?, ?)`, [steamID, bundleCode, price])
+        return !!res?.affectedRows
+    } catch (e) {
+        console.log(e)
+        return false
+    }
 }
 
 export async function connectAccounts(steamID: string, discordID: string): Promise<boolean> {
-    const res = await api_v4.execute(`INSERT INTO steam_discord_ids (steam_id, discord_id) VALUES (?, ?)`, [steamID, discordID])
-    
-    return !!res?.affectedRows
+    try {
+        const res = await api_v4.execute(`INSERT INTO steam_discord_ids (steam_id, discord_id) VALUES (?, ?)`, [steamID, discordID])
+        return !!res?.affectedRows
+    } catch (e) {
+        console.log(e)
+        return false
+    }
 }
 
 // export async function getSteamIDFromToken(token: string): Promise<string> {
